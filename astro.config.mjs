b0e4157fc7afc1b02bbba597145094051a9ef3a9
Tomas Apochian <tomasapochian@gmail.com>
2024-08-18 12:54:30 -0300

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +12,8 @@ export default defineConfig({
     defaultLocale: 'es',
     locales: ["es", "en"]
   },
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 })
